c620072fc59fe7de56a6a1f7dbd93ad0f6178e9b
harshh06 <harsh66287@gmail.com>
2021-05-27 18:47:55 +0530

    product details coming for db.. added productReducer

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,6 +1,9 @@
 // handle state for product list ..
 
 import {
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
@@ -17,6 +20,26 @@ export const productListReducer = (state = { products: [] }, action) => {
       return { loading: false, products: action.payload };
     // fail
     case PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const productDetailsReducer = (
+  state = { product: { reviews: [] } },
+  action
+) => {
+  // takes two things as arg.. : (initialState, action)
+  switch (action.type) {
+    // request
+    case PRODUCT_DETAILS_REQUEST:
+      return { loading: true, ...state };
+    // success
+    case PRODUCT_DETAILS_SUCCESS:
+      return { loading: false, product: action.payload };
+    // fail
+    case PRODUCT_DETAILS_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
