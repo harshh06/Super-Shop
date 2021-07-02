0b0d6fde7d64291c25fe71b7c286204271373bbf
harshh06 <harsh66287@gmail.com>
2021-07-02 16:44:33 +0530

    prepare for deployement

--- backend/server.js
+++ backend/server.js
@@ -13,27 +13,35 @@ connectDB();
 
 const app = express();
 
-if (process.env.NODE_ENV == "development") {
+if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
 app.use(express.json()); // allow json data in body
-
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
 
-const __dirname = path.resolve();
-app.use("./uploads", express.static(path.join(__dirname, "./uploads")));
-
 app.get("/api/config/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID);
 });
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(
